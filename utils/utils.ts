4129c21c44a85c072e3f4b2be231a15fe9936de1
09982 <09982.misaka@gmail.com>
2023-08-04 10:16:52 +0800

    style: format code

--- utils/utils.ts
+++ utils/utils.ts
@@ -182,3 +182,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:39.542694
 
 // Auto-generated comment - 2025-07-13 14:24:39.681731
+
+// Auto-generated comment - 2025-07-13 14:24:48.282399
