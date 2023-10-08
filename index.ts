480e70385a1772cca071fc0b01fd60fb6ed9e702
09982 <09982.misaka@gmail.com>
2023-10-08 22:36:11 +0800

    fix: resolve cross-browser compatibility

--- index.ts
+++ index.ts
@@ -188,3 +188,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:17.189520
 
 // Auto-generated comment - 2025-07-13 14:23:17.199034
+
+// Auto-generated comment - 2025-07-13 14:23:17.215987
