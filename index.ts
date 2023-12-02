d2c4d23a36fecd766699d1b98f1d90e2f629c207
09982 <09982.misaka@gmail.com>
2023-12-02 10:43:36 +0800

    docs: update configuration guide

--- index.ts
+++ index.ts
@@ -274,3 +274,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:20.873143
 
 // Auto-generated comment - 2025-07-13 14:23:21.024912
+
+// Auto-generated comment - 2025-07-13 14:23:21.171542
