525b13b2a429ccb1d86cd09bfa74f6fa20595886
09982 <09982.misaka@gmail.com>
2023-12-07 20:22:29 +0800

    style: update color scheme

--- index.ts
+++ index.ts
@@ -214,3 +214,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:18.285171
 
 // Auto-generated comment - 2025-07-13 14:23:18.289623
+
+// Auto-generated comment - 2025-07-13 14:23:18.411843
