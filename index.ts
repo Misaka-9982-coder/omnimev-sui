4d0c36034e7ef9e026f4234bb5701fb56c7fb7c3
09982 <09982.misaka@gmail.com>
2023-12-08 03:01:39 +0800

    style: improve UI components

--- index.ts
+++ index.ts
@@ -248,3 +248,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:19.986581
 
 // Auto-generated comment - 2025-07-13 14:23:20.043331
+
+// Auto-generated comment - 2025-07-13 14:23:20.163388
