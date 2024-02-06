ce1c3ae9e4c9505fe194b3860da82c07aabc061d
09982 <09982.misaka@gmail.com>
2024-02-07 07:45:56 +0800

    fix: resolve memory leak

--- index.ts
+++ index.ts
@@ -184,3 +184,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:16.895970
 
 // Auto-generated comment - 2025-07-13 14:23:17.158867
+
+// Auto-generated comment - 2025-07-13 14:23:17.189520
