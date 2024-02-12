8f104ae6952da5ccc6e5f8e104e7c3c77f67d588
09982 <09982.misaka@gmail.com>
2024-02-13 01:43:10 +0800

    fix: handle edge case in payment

--- utils/utils.ts
+++ utils/utils.ts
@@ -200,3 +200,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:49.475760
 
 // Auto-generated comment - 2025-07-13 14:24:49.514527
+
+// Auto-generated comment - 2025-07-13 14:24:49.776835
