039488bf8e74c4187539ac4a1448f8dcf89ea849
09982 <09982.misaka@gmail.com>
2024-02-25 06:38:22 +0800

    test: add integration tests

--- utils/utils.ts
+++ utils/utils.ts
@@ -150,3 +150,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:37.143904
 
 // Auto-generated comment - 2025-07-13 14:24:37.256805
+
+// Auto-generated comment - 2025-07-13 14:24:38.004383
