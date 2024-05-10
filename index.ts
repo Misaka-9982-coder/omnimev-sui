8bbc752be96405b3ffc685310c193b8d1b6ba613
09982 <09982.misaka@gmail.com>
2024-05-10 10:50:19 +0800

    style: improve accessibility

--- index.ts
+++ index.ts
@@ -198,3 +198,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:17.516916
 
 // Auto-generated comment - 2025-07-13 14:23:17.621673
+
+// Auto-generated comment - 2025-07-13 14:23:17.652060
