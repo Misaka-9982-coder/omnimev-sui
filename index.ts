635eedf83e34032addb2d2f036ca00c48e159803
09982 <09982.misaka@gmail.com>
2024-05-17 16:03:27 +0800

    feat: implement dark mode

--- index.ts
+++ index.ts
@@ -194,3 +194,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:17.354093
 
 // Auto-generated comment - 2025-07-13 14:23:17.482182
+
+// Auto-generated comment - 2025-07-13 14:23:17.516916
