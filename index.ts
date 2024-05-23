9d5e2557a4d0725fcd57cbba323fa23e8663a65e
09982 <09982.misaka@gmail.com>
2024-05-24 02:10:38 +0800

    fix: correct validation logic

--- index.ts
+++ index.ts
@@ -228,3 +228,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:18.960502
 
 // Auto-generated comment - 2025-07-13 14:23:19.092126
+
+// Auto-generated comment - 2025-07-13 14:23:19.208447
