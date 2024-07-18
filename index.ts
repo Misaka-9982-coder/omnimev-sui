cbd30336310f129a17809a24078a43fb45a7aa0c
09982 <09982.misaka@gmail.com>
2024-07-18 13:31:04 +0800

    chore: update build scripts

--- index.ts
+++ index.ts
@@ -354,3 +354,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:24:49.784507
 
 // Auto-generated comment - 2025-07-13 14:24:49.803719
+
+// Auto-generated comment - 2025-07-13 14:24:50.043759
