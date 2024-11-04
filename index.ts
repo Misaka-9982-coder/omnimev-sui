e58fbfccf8a3c878053f0e4772f7d8e9fa0163f1
09982 <09982.misaka@gmail.com>
2024-11-04 17:01:12 +0800

    test: add end-to-end tests

--- index.ts
+++ index.ts
@@ -180,3 +180,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:16.772989
 
 // Auto-generated comment - 2025-07-13 14:23:16.891542
+
+// Auto-generated comment - 2025-07-13 14:23:16.895970
