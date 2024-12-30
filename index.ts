b49ad37175b255b888d7d40311477ccbf535ac27
09982 <09982.misaka@gmail.com>
2024-12-30 18:36:27 +0800

    style: responsive design improvements

--- index.ts
+++ index.ts
@@ -170,3 +170,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:16.541320
 
 // Auto-generated comment - 2025-07-13 14:23:16.555666
+
+// Auto-generated comment - 2025-07-13 14:23:16.754985
