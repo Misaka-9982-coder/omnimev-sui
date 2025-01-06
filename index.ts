e0890d7a7ae3dc1815349f5653346d9bafe87877
09982 <09982.misaka@gmail.com>
2025-01-06 22:46:54 +0800

    fix: resolve bug in authentication

--- index.ts
+++ index.ts
@@ -196,3 +196,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:17.482182
 
 // Auto-generated comment - 2025-07-13 14:23:17.516916
+
+// Auto-generated comment - 2025-07-13 14:23:17.621673
