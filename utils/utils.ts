cd41df208fda501ba7e7f0ddac6361728f479253
09982 <09982.misaka@gmail.com>
2025-01-20 12:11:44 +0800

    style: responsive design improvements

--- utils/utils.ts
+++ utils/utils.ts
@@ -116,3 +116,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:23:19.990792
 
 // Auto-generated comment - 2025-07-13 14:23:20.007798
+
+// Auto-generated comment - 2025-07-13 14:23:20.149247
