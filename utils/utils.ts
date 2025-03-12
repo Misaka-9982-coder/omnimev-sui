3e1ad0de912a544439474a400a55c7e5bb444d5c
09982 <09982.misaka@gmail.com>
2025-03-13 06:20:08 +0800

    refactor: optimize database queries

--- utils/utils.ts
+++ utils/utils.ts
@@ -166,3 +166,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:38.561875
 
 // Auto-generated comment - 2025-07-13 14:24:38.689529
+
+// Auto-generated comment - 2025-07-13 14:24:38.700633
