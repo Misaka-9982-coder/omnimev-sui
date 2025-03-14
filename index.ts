9e28cfb4013bfac8f371d4fcef5992606943e177
09982 <09982.misaka@gmail.com>
2025-03-14 21:49:48 +0800

    refactor: clean up legacy code

--- index.ts
+++ index.ts
@@ -284,3 +284,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:21.491993
 
 // Auto-generated comment - 2025-07-13 14:23:21.864499
+
+// Auto-generated comment - 2025-07-13 14:23:21.888736
