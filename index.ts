70ea96a0ab9c5f2d4507f692e68935285185c2f5
09982 <09982.misaka@gmail.com>
2025-04-05 21:13:12 +0800

    fix: handle edge case in payment

--- index.ts
+++ index.ts
@@ -358,3 +358,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:24:50.043759
 
 // Auto-generated comment - 2025-07-13 14:24:50.048043
+
+// Auto-generated comment - 2025-07-13 14:24:50.173577
