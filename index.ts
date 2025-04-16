074eee34911a25a930322ddb3960c0f779bb04a6
09982 <09982.misaka@gmail.com>
2025-04-16 15:03:39 +0800

    refactor: optimize database queries

--- index.ts
+++ index.ts
@@ -342,3 +342,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:24:49.212322
 
 // Auto-generated comment - 2025-07-13 14:24:49.217800
+
+// Auto-generated comment - 2025-07-13 14:24:49.467173
