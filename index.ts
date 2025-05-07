9c87bd85f609d32df476d08a32e0d9990f13db79
09982 <09982.misaka@gmail.com>
2025-05-07 08:22:21 +0800

    feat: add multi-language support

--- index.ts
+++ index.ts
@@ -222,3 +222,5 @@ monitorAndExecuteTrades(BigInt(200000000), SUI_TOKEN, USDC_TOKEN, 20000);
 // Auto-generated comment - 2025-07-13 14:23:18.420707
 
 // Auto-generated comment - 2025-07-13 14:23:18.939576
+
+// Auto-generated comment - 2025-07-13 14:23:18.956309
