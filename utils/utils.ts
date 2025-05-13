969efd4bece9a754e4f78337ac03a197b393cb7f
09982 <09982.misaka@gmail.com>
2025-05-13 20:53:50 +0800

    test: add unit tests

--- utils/utils.ts
+++ utils/utils.ts
@@ -154,3 +154,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:38.004383
 
 // Auto-generated comment - 2025-07-13 14:24:38.013182
+
+// Auto-generated comment - 2025-07-13 14:24:38.047161
