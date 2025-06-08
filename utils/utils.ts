bdb6398bccfeffea01cf22148ffd9460c9e92631
09982 <09982.misaka@gmail.com>
2025-06-09 04:31:48 +0800

    fix: resolve memory leak

--- utils/utils.ts
+++ utils/utils.ts
@@ -172,3 +172,5 @@ export function div(numerator: bigint, denominator: bigint, decimalPlaces: numbe
 // Auto-generated comment - 2025-07-13 14:24:38.822181
 
 // Auto-generated comment - 2025-07-13 14:24:38.833723
+
+// Auto-generated comment - 2025-07-13 14:24:38.945170
